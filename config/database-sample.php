74fb1e9db990f4a4149decf743a88f0c3d3ce0f0
DenzoNL <dennis@ruun.nl>
2015-05-01 11:41:48 +0200

    Added sample database config and .gitignore

--- config/database-sample.php
+++ config/database-sample.php
@@ -55,9 +55,9 @@ return [
 		'mysql' => [
 			'driver'    => 'mysql',
 			'host'      => env('DB_HOST', 'localhost'),
-			'database'  => env('DB_DATABASE', 'unrealmetrics'),
-			'username'  => env('DB_USERNAME', 'unrealmetrics'),
-			'password'  => env('DB_PASSWORD', 'LWuA38BSPKn63RXL'),
+			'database'  => env('DB_DATABASE', 'forge'),
+			'username'  => env('DB_USERNAME', 'forge'),
+			'password'  => env('DB_PASSWORD', ''),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
